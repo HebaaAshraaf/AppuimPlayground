24cafa5dcb3587312c1d0aed693ec75f45586833
Hoba <hebaafouaad@gmail.com>
2020-05-28 18:11:10 +0200

    My First Mobile Test Automation Project

--- src/test/java/Tests/TestMobWebApp.java
+++ src/test/java/Tests/TestMobWebApp.java
@@ -2,6 +2,7 @@ package Tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -15,17 +16,25 @@ public class TestMobWebApp {
     public void launchingAUT() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
 
-        caps.setCapability("browserName", "chrome");
-        caps.setCapability("appuim-version", "1.17.1");
+
+        caps.setCapability("appuim-version", "1.15.1");
         caps.setCapability("Version", "7.0");
         caps.setCapability("deviceName", "emulator-5554");
         caps.setCapability("platformName", "Android");
+        caps.setCapability("automationName", "Appium");
+        caps.setCapability("appPackage", "com.android.calculator2");
+        caps.setCapability("appActivity", "com.android.calculator2.Calculator");
+        caps.setCapability("orientationg", "PORTRAIT");
+        caps.setCapability("noReset", "true");
 
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), caps);
     }
 
     @Test
     public void runFirstScript() {
-        driver.get("http://appium.io/");
+        driver.findElementById("digit_2").click();
+        driver.findElementById("op_add").click();
+        driver.findElementById("digit_2").click();
+        Assert.assertEquals(driver.findElementById("result").getText(),"4");
     }
 }
